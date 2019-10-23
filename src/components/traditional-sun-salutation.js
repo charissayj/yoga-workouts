159187e5cf6bd23650d23fcbe9e959e78031efbe
Charissa Johnson <charissa.y.johnson@gmail.com>
2019-10-22 22:00:26 -0500

    use function instead of class components

--- src/components/traditional-sun-salutation.js
+++ src/components/traditional-sun-salutation.js
@@ -1,35 +1,26 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class TraditionalSunSalutation extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      steps: []
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Traditional Sun Salutation</h1>
-        <h3>Warm-Up</h3>
-        <p>
-          Warm up your entire body with this classic version of the Sun
-          Salutation. Follow the steps below and repeat for 3 - 10 rounds for a
-          complete warm-up
-        </p>
-        <label>Steps:</label>
-        <ol>
-          <li>
-            Stand with your feet hip distance apart. Exhale and bring your hands
-            to prayer
-          </li>
-          <li>Inhale and reach up, arching your back</li>
-          <li>Inhale and reach up, arching your back</li>
-        </ol>
-      </div>
-    );
-  }
+function TraditionalSunSalutation() {
+  return (
+    <div>
+      <h1>Traditional Sun Salutation</h1>
+      <h3>Warm-Up</h3>
+      <p>
+        Warm up your entire body with this classic version of the Sun
+        Salutation. Follow the steps below and repeat for 3 - 10 rounds for a
+        complete warm-up
+      </p>
+      <label>Steps:</label>
+      <ol>
+        <li>
+          Stand with your feet hip distance apart. Exhale and bring your hands
+          to prayer
+        </li>
+        <li>Inhale and reach up, arching your back</li>
+        <li>Inhale and reach up, arching your back</li>
+      </ol>
+    </div>
+  );
 }
- 
+
 export default TraditionalSunSalutation;
